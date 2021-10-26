97893e7530df198ecf73d739049cf541ee0f5c7b
Pham Vuong <34092076+phamvv@users.noreply.github.com>
2021-10-26 13:14:30 +0700

    Initial commit

---
+++ Model/MyDataContext.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TestAPI_SITV.Model
+{
+    public class MyDataContext : DbContext
+    {
+        public DbSet<NhanVien> nhanViens { set; get; }        // khai báo bảng Nhân Viên từ Model
+
+
+        // chuỗi kết nối đến database với tên MyDB sẽ làm việc
+        public const string ConnectStrring = @"Data Source=localhost,1433;Initial Catalog=MyDB;User ID=SA;Password=Password123";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectStrring);                // thiết lập kết nối đến database
+            optionsBuilder.UseLoggerFactory(GetLoggerFactory());        // bật logger
+        }
+
+        private ILoggerFactory GetLoggerFactory()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder =>
+                    builder.AddConsole()
+                           .AddFilter(DbLoggerCategory.Database.Command.Name,
+                                    LogLevel.Information));
+            return serviceCollection.BuildServiceProvider()
+                    .GetService<ILoggerFactory>();
+        }
+
+    }
+
+}
---
+++ Model/NhanVien.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI_SITV.Model
+{
+    [Table("article")]
+    public class NhanVien
+    {
+        [Key]   
+        public int ID { get; set; }
+
+        [StringLength(50)]
+        public string MaNhanVien { get; set; }
+
+        [StringLength(100)]
+        public string HoTenNhanVien { get; set; }
+
+        [StringLength(500)]
+        public string DiaChi { get; set; }
+
+
+        [StringLength(50)]
+        public string PhongBan { get; set; }
+
+    }
+}
